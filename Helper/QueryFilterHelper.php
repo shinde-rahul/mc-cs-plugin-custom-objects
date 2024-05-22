0bf73d1a6a5b2f3dc21bf2d27b0041d7b20958a1
aarohiprasad <aarohi.prasad@acquia.com>
2024-05-22 12:27:56 +0530

    fixed incorrect merge

--- Helper/QueryFilterHelper.php
+++ Helper/QueryFilterHelper.php
@@ -93,7 +93,6 @@ class QueryFilterHelper
                 $tableAlias,
                 $filter->getOperator(),
                 $valueParameter,
-                $valueParameter,
                 $filter->getParameterValue()
             );
 
