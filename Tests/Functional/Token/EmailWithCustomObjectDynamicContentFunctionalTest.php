9a9c7d3fcaa770f0dfdea7246435ef611386178e
Avikarsha Saha <avikarsha.saha@acquia.com>
2024-06-20 11:18:54 +0530

    test case

--- Tests/Functional/Token/EmailWithCustomObjectDynamicContentFunctionalTest.php
+++ Tests/Functional/Token/EmailWithCustomObjectDynamicContentFunctionalTest.php
@@ -2,24 +2,27 @@
 
 declare(strict_types=1);
 
-namespace MauticPlugin\CustomObjectsBundle\Tests\Functional;
+namespace MauticPlugin\CustomObjectsBundle\Tests\Functional\Token;
 
 use DateTime;
 use Mautic\CoreBundle\Test\MauticMysqlTestCase;
 use Mautic\EmailBundle\Entity\Email;
+use Mautic\EmailBundle\Entity\Stat;
+use Mautic\EmailBundle\Entity\StatRepository;
+use Mautic\EmailBundle\Model\EmailModel;
 use Mautic\LeadBundle\Entity\Lead;
 use MauticPlugin\CustomObjectsBundle\Entity\CustomItem;
 use MauticPlugin\CustomObjectsBundle\Model\CustomFieldValueModel;
 use MauticPlugin\CustomObjectsBundle\Model\CustomItemModel;
 use MauticPlugin\CustomObjectsBundle\Tests\Functional\DataFixtures\Traits\CustomObjectsTrait;
-use MauticPlugin\CustomObjectsBundle\Tests\ProjectVersionTrait;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class PreviewFunctionalTest extends MauticMysqlTestCase
+class EmailWithCustomObjectDynamicContentFunctionalTest extends MauticMysqlTestCase
 {
     use CustomObjectsTrait;
-    use ProjectVersionTrait;
 
     /**
      * @var CustomItemModel
@@ -39,12 +42,8 @@ class PreviewFunctionalTest extends MauticMysqlTestCase
         $this->customFieldValueModel = self::$container->get('mautic.custom.model.field.value');
     }
 
-    public function testPreviewPageCODynamicContent(): void
+    public function testEmailWithCustomObjectDynamicContent(): void
     {
-        if (!$this->isCloudProject()) {
-            $this->markTestSkipped('Not supported in 4.4');
-        }
-
         $customObject = $this->createCustomObjectWithAllFields(self::$container, 'Car');
         $customItem   = new CustomItem($customObject);
 
@@ -52,11 +51,12 @@ class PreviewFunctionalTest extends MauticMysqlTestCase
         $this->customFieldValueModel->createValuesForItem($customItem);
 
         $textValue       = $customItem->findCustomFieldValueForFieldAlias('text-test-field');
-        $textValue->setValue('abracadabra');
+        $textValue->setValue('Tata');
 
         $customItem = $this->customItemModel->save($customItem);
 
-        $lead  = $this->createLead();
+        $lead1  = $this->createLead('nexon@tata.com');
+        $lead2  = $this->createLead('noitem@tata.com');
         $email = $this->createEmail();
         $email->setDynamicContent(
             [
@@ -83,8 +83,17 @@ class PreviewFunctionalTest extends MauticMysqlTestCase
                                     'field'    => 'cmf_'.$textValue->getCustomField()->getId(),
                                     'object'   => 'custom_object',
                                     'type'     => 'text',
-                                    'filter'   => 'abracadabra',
-                                    'display'  => $customObject->getAlias().':text-test-field',
+                                    'filter'   => 'Tata',
+                                    'display'  => $customObject->getAlias().': text-test-field',
+                                    'operator' => '=',
+                                ],
+                                [
+                                    'glue'     => 'and',
+                                    'field'    => 'cmo_'.$customObject->getId(),
+                                    'object'   => 'custom_object',
+                                    'type'     => 'text',
+                                    'filter'   => 'Nexon',
+                                    'display'  => $customObject->getAlias(),
                                     'operator' => '=',
                                 ],
                             ],
@@ -96,31 +105,10 @@ class PreviewFunctionalTest extends MauticMysqlTestCase
         $this->em->persist($email);
         $this->em->flush();
 
-        $this->customItemModel->linkEntity($customItem, 'contact', (int) $lead->getId());
+        $this->customItemModel->linkEntity($customItem, 'contact', (int) $lead1->getId());
 
-        $url                    = "/email/preview/{$email->getId()}";
-        $urlWithContact         = "{$url}?contactId={$lead->getId()}";
-        $contentNoContactInfo   = 'Default Dynamic Content';
-        $contentWithContactInfo = 'Nexon Dynamic Content';
-
-        // Anonymous visitor
-        $this->assertPageContent($url, $contentNoContactInfo);
-        $this->assertPageContent($urlWithContact, $contentNoContactInfo);
-
-        $this->loginUser('admin');
-
-        // Admin user
-        $this->assertPageContent($url, $contentNoContactInfo);
-        $this->assertPageContent($urlWithContact, $contentWithContactInfo);
-    }
-
-    private function assertPageContent(string $url, string ...$expectedContents): void
-    {
-        $crawler = $this->client->request(Request::METHOD_GET, $url);
-        self::assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
-        foreach ($expectedContents as $expectedContent) {
-            self::assertStringContainsString($expectedContent, $crawler->text());
-        }
+        $this->sendAndAssetText($email, $lead1, 'Nexon Dynamic Content');
+        $this->sendAndAssetText($email, $lead2, 'Default Dynamic Content');
     }
 
     private function createEmail(bool $publicPreview = true): Email
@@ -137,12 +125,66 @@ class PreviewFunctionalTest extends MauticMysqlTestCase
         return $email;
     }
 
-    private function createLead(): Lead
+    private function createLead(string $email): Lead
     {
         $lead = new Lead();
-        $lead->setEmail('test@domain.tld');
+        $lead->setEmail($email);
         $this->em->persist($lead);
 
         return $lead;
+    }
+
+    /**
+     * @param EmailModel $emailModel
+     * @param Email $email
+     * @param Lead $lead
+     * @param StatRepository $emailStatRepository
+     * @return void
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function sendAndAssetText(Email $email, Lead $lead, string $matchText): void
+    {
+        /** @var EmailModel $emailModel */
+        $emailModel = self::$container->get('mautic.email.model.email');
+        $emailModel->sendEmail(
+            $email,
+            [
+                [
+                    'id' => $lead->getId(),
+                    'email' => $lead->getEmail(),
+                    'firstname' => $lead->getFirstname(),
+                    'lastname' => $lead->getLastname(),
+                ],
+            ]
+        );
+
+        /** @var StatRepository $emailStatRepository */
+        $emailStatRepository = $this->em->getRepository(Stat::class);
+
+        /** @var Stat|null $emailStat */
+        $emailStat = $emailStatRepository->findOneBy(
+            [
+                'email' => $email->getId(),
+                'lead' => $lead->getId(),
+            ]
+        );
+
+        Assert::assertNotNull($emailStat);
+
+        $crawler = $this->client->request(Request::METHOD_GET, "/email/view/{$emailStat->getTrackingHash()}");
+
+        $body = $crawler->filter('body');
+
+        // Remove the tracking tags that are causing troubles with different Mautic configurations.
+        $body->filter('a,img,div')->each(function (Crawler $crawler) {
+            foreach ($crawler as $node) {
+                $node->parentNode->removeChild($node);
+            }
+        });
+
+        Assert::assertStringContainsString(
+            $matchText,
+            $body->html()
+        );
     }
 }
